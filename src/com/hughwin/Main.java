4530376765696bd773efe8b615851c5cd1bfa63e
Hugh Winchester <hugh.winchester@gmail.com>
2019-12-19 16:46:15 +0000

    Added functionaility that enables the files in a directory to be read.
    Added file globber to search the contents of the directory

--- src/com/hughwin/Main.java
+++ src/com/hughwin/Main.java
@@ -1,24 +1,38 @@
 package com.hughwin;
 
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static java.nio.file.Files.newDirectoryStream;
+import static java.nio.file.Files.notExists;
+
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
         System.out.println(args.length);
         if (args.length == 0) {
-            throw new Exception("Please specify a file");
+            throw new IllegalArgumentException("Please specify a file");
         }
         if (args.length > 1){
-            throw new Exception(("You have specified too many files"));
+            throw new IllegalArgumentException(("You have specified too many files"));
         }
         Path path = Paths.get(args[0]);
-        if (Files.notExists(path)){
-            throw new FileNotFoundException("File not found at location");
+        if (notExists(path)){
+            throw new FileNotFoundException("File not found at " + path);
         }
+        System.out.println("File found!");
+        try (DirectoryStream<Path> stream =  newDirectoryStream(path, "*.mp3")){
+            for (Path entry : stream){
+                System.out.println(entry.getFileName());
+            }
+        }catch (IOException x){
+            System.err.println();
+        }
+
 
     }
 }
