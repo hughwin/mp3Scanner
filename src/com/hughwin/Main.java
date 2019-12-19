d070166ed0712b1ae89957edc0bad2f7e773ec4d
Hugh Winchester <hugh.winchester@gmail.com>
2019-12-19 16:18:50 +0000

    Arguements assessed to see whether file directory exisits

--- src/com/hughwin/Main.java
+++ src/com/hughwin/Main.java
@@ -1,11 +1,24 @@
 package com.hughwin;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) throws Exception{
-	for (String x : args){
-	    System.out.println(x);
-	    System.out.println("Process completed");
-    }
+        System.out.println(args.length);
+        if (args.length == 0) {
+            throw new Exception("Please specify a file");
+        }
+        if (args.length > 1){
+            throw new Exception(("You have specified too many files"));
+        }
+        Path path = Paths.get(args[0]);
+        if (Files.notExists(path)){
+            throw new FileNotFoundException("File not found at location");
+        }
+
     }
 }
